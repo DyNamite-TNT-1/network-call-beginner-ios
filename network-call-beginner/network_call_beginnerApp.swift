22e4e2758bc559141fc126ae5fc53edba5a82366
lemauanhduc <tdhuaco372001@gmail.com>
2023-11-24 11:25:21 +0700

    Initial Commit

---
+++ network-call-beginner/network_call_beginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  network_call_beginnerApp.swift
+//  network-call-beginner
+//
+//  Created by hanbiro on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct network_call_beginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
